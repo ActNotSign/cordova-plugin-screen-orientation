24e659e103504009aa2b5004e82f851684fe189e
ActNotSign <yuxing_act@163.com>
2022-10-09 15:10:34 +0800

    Update CDVOrientation.m

--- src/ios/CDVOrientation.m
+++ src/ios/CDVOrientation.m
@@ -75,7 +75,6 @@
                 if (_lastOrientation != UIInterfaceOrientationUnknown) {
                     [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:_lastOrientation] forKey:@"orientation"];
                     ((void (*)(CDVViewController*, SEL, NSMutableArray*))objc_msgSend)(vc,selector,result);
-                    [UINavigationController attemptRotationToDeviceOrientation];
                     if (@available(iOS 16.0, *)) {
                           #if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_15_5 // Xcode 14 and iOS 16, or greater
                               [self.viewController setNeedsUpdateOfSupportedInterfaceOrientations];
